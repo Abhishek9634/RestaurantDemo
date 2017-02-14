8b685f1470b336ca834aacdddb86eebece824202
Abhishek Thapliyal <Abhishek@Abhisheks-MacBook-Air.local>
2017-02-14 11:03:57 +0530

    Added 4sq fw #2

--- RestaurantDemo/RestaurantDemo/ViewController.swift
+++ RestaurantDemo/RestaurantDemo/ViewController.swift
@@ -8,9 +8,20 @@
 
 import UIKit
 import QuadratTouch
+import CoreLocation
+import Foundation
+import MapKit
+
+typealias JSONParameters = [String: AnyObject]
 
 class ViewController: UIViewController {
 
+    var session: Session!
+    var location: CLLocation!
+    var venues: [JSONParameters]!
+    let distanceFormatter = MKDistanceFormatter()
+    var currentTask: Task?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +32,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+    
+        var parameters = [Parameter.query:"Burgers"]
+        parameters += self.location.parameters()
+        let searchTask = session.venues.search(parameters) {
+            (result) -> Void in
+            if let response = result.response {
+                self.venues = response["venues"] as! [JSONParameters]?
+            }
+        }
+        searchTask.start()
+        
+    }
 
+}
+
+extension CLLocation {
+    func parameters() -> Parameters {
+        let ll      = "\(self.coordinate.latitude),\(self.coordinate.longitude)"
+        let llAcc   = "\(self.horizontalAccuracy)"
+        let alt     = "\(self.altitude)"
+        let altAcc  = "\(self.verticalAccuracy)"
+        let parameters = [
+            Parameter.ll:ll,
+            Parameter.llAcc:llAcc,
+            Parameter.alt:alt,
+            Parameter.altAcc:altAcc
+        ]
+        return parameters
+    }
 }
 
